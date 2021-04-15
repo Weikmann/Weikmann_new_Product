5e37dd1d4a56d256975a1eea5dd916835e7e6991
MartinW <wei21921@spengergasse.at>
2021-04-15 12:47:13 +0200

    Weikmann: added Class Product

---
+++ Product.java
@@ -0,0 +1,22 @@
+﻿public class Prodcut
+{
+      private String name;
+
+      public Product()
+      {
+           setName("Coca Cola");
+      }
+
+
+      public String getName()
+      {
+           return name;
+      }
+
+      public String setName()
+      {
+           this.name = name;
+      }
+
+
+}
